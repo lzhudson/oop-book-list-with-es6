b3f559b22b193183e4a57c54214efe3d3347c3dc
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-17 10:47:48 -0300

    fix: update entry path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 /** @type {import('webpack').Configuration} */
 
 module.exports = {
-  entry: path.resolve(__dirname, 'app.js'),
+  entry: path.resolve(__dirname, 'src', 'app.js'),
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
