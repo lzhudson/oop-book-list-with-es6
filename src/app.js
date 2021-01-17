c4e4b32fede2c149380ea7e399caaa09d4ce60a9
Hudson Holanda <hudsonholanda55@gmail.com>
2021-01-17 10:57:31 -0300

    style: remove comments and console statement

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ class Book {
 class UI {
   addBookToList(book){
     const list = document.getElementById('book-list');
-    // Create tr element
     const row = document.createElement('tr');
-    
-    // Insert cols
     row.innerHTML = `
     <td>${book.title}</td>
     <td>${book.author}</td>
@@ -21,21 +18,12 @@ class UI {
     list.appendChild(row);
   }
   showAlert(message, className) {
-    // Create div
     const div = document.createElement('div');
-    // Add classes
     div.className = `alert ${className}`;
-    // Add text
     div.appendChild(document.createTextNode(message));
-    
-    // Get parent
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
-    
-    // Insert Alert  
     container.insertBefore(div, form);
-    
-    // Timeout after 3 sec
     setTimeout(function(){
       document.querySelector('.alert').remove();
     }, 3000);
@@ -53,43 +41,25 @@ class UI {
     document.getElementById('isbn').value = '';
   }
 }
-
-// Event Listener add book
 document.getElementById('book-form').addEventListener('submit', function(e){
-  // Get Form Values
   const title = document.getElementById('title').value,
         author = document.getElementById('author').value,
         isbn = document.getElementById('isbn').value
-
-  // Instantiate book
   const book = new Book(title, author, isbn);
-
-  // Instantiate UI
   const ui = new UI();
-  console.log(ui);
-  // Validae
   if(title === '' || author === '' || isbn === ''){
-    // Error alert
     ui.showAlert('Please fill in all fields', 'error');
   } else {
-    // Add book to list
     ui.addBookToList(book);
-
-    // Show alert
     ui.showAlert('Book Insert Sucess', 'success');
-
-    // Clear Fields
     ui.clearFields();
   }
   e.preventDefault();
 });
 
-// Event Listener for delete
 document.getElementById('book-list').addEventListener('click', function(e){
   const ui = new UI();
-  // Delete Book
   ui.deleteBook(e.target);
-  // Show Message
   ui.showAlert('Book Removed!', 'success');
   e.preventDefault();
 });
